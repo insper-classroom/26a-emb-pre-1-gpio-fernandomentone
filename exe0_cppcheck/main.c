bfe522c32dfa902573b81902d651e0178a8906a2
fernandomentone <fernandomm9@al.insper.edu.br>
2026-02-23 21:54:59 -0300

    exe0 e exe1

--- exe0_cppcheck/main.c
+++ exe0_cppcheck/main.c
@@ -3,18 +3,25 @@
 
 #define NUM_ELEMENTS 5
 
-int multiplyNumbers(int x, int y) {
-    int product = x * y;
+static int multiplyNumbers(int x, int y) {
+    return x * y;
 }
 
 int main(void) {
-    int a, b = 5; //uninitvar, legacyUninitvar
+    int a = 0;
+    int b = 5; //uninitvar, legacyUninitvar
     int result = multiplyNumbers(a, b);
 
     int arr[NUM_ELEMENTS];
-    for(int i = 0; i <= NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
+    for(int i = 0; i < NUM_ELEMENTS; i++) { //arrayIndexOutOfBounds
         arr[i] = i;
     }
 
+    int sum = result;
+    for (int i = 0; i < NUM_ELEMENTS; i++) {
+        sum += arr[i];
+    }
+    printf("%d\n", sum);
+    
     return 0;
 }
